e3f803861b18e720c747f0b333ac6298e4bdbfc9
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-08-17 17:40:47 +0530

    Update DemoApplication.java

--- src/main/java/com/nuvepro/demo/DemoApplication.java
+++ src/main/java/com/nuvepro/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String getHomePage()
 	{
 		return "index";
