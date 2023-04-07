0452baf68c3c86a7878513c936d7e6f7154b5538
yadav3500 <122116050+yadav3500@users.noreply.github.com>
2023-04-07 14:56:28 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,9 @@
 echo hello world
 ls
+version= $(cat number.txt)
+echo $version
 docker stop nuvepro
 docker rm -f nuvepro
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418843764796.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-${BUILD_NO}
-docker run -d -p 8080:8083 --name nuvepro 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-${BUILD_NO}
+docker pull 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$version
+docker run -d -p 8080:8083 --name nuvepro 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$version
