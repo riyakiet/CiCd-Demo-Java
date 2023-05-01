60f1da6ea43d11db1c8543029352b5def576104e
yadav3500 <122116050+yadav3500@users.noreply.github.com>
2023-05-01 17:03:57 +0530

    Update DemoApplication.java

--- src/main/java/com/nuvepro/demo/DemoApplication.java
+++ src/main/java/com/nuvepro/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.nuvepro.demo;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -7,17 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping("/index")
-	public String getHomePage()
-	{
-		return "index";
-	}
+    @GetMapping("/index")
+    public String getHomePage()
+    {
+        return "index";
+    }
+    @GetMapping("/status")
+    public ResponseEntity<String> getAll(){
+        return new ResponseEntity<>("Okay",HttpStatus.OK);
+    }
 
 }
