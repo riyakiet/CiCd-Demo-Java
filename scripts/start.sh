3dfcba3269dcf22ba050b4561c07d7c4e140cf99
yadav3500 <122116050+yadav3500@users.noreply.github.com>
2023-11-14 22:41:41 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
 echo hello world
 cd /home/ubuntu/
 ls
-value=`cat number.txt`
-echo $value
-docker stop nuvepro
-docker rm -f nuvepro
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418843764796.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
-docker run -d -p 8080:8083 --name nuvepro 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
+# value=`cat number.txt`
+# echo $value
+# docker stop nuvepro
+# docker rm -f nuvepro
+# aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418843764796.dkr.ecr.ap-south-1.amazonaws.com
+# docker pull 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
+# docker run -d -p 8080:8083 --name nuvepro 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
+sudo nohup java -jar demo-0.0.1-SNAPSHOT.jar &
