92aec8fa169124e0297670b4e6c2b07fab3f2d57
yadav3500 <122116050+yadav3500@users.noreply.github.com>
2023-11-18 23:10:18 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,4 +12,5 @@ ls
 # aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418843764796.dkr.ecr.ap-south-1.amazonaws.com
 # docker pull 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
 # docker run -d -p 8080:8083 --name nuvepro 418843764796.dkr.ecr.ap-south-1.amazonaws.com/nuvepro:nuvepro-$value
-sudo nohup java -jar demo-0.0.1-SNAPSHOT.jar &
+#sudo nohup java -jar demo-0.0.1-SNAPSHOT.jar &
+nohup java -jar demo-0.0.1-SNAPSHOT.jar > output.log 2>&1 &
